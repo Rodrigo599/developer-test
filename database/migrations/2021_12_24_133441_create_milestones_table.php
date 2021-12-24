743d1fd163c0535950b8a159162d0c88f709707d
Rodrigo Tagliari <rodrigotagliarii@gmail.com>
2021-12-24 12:45:01 -0300

    Creating Migrations

---
+++ database/migrations/2021_12_24_133441_create_milestones_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Achievement;
+use App\Models\Milestone;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMilestonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('milestones', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Achievement::class);
+            $table->string('model');
+            $table->integer('milestone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('milestones');
+    }
+}
